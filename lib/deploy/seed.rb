850852f025ab9973fbbf327c277be964eccf3ff1
Claudio <claudio.leiva.r@gmail.com>
2021-09-08 12:16:37 -0300

    prueba6

--- lib/deploy/seed.rb
+++ lib/deploy/seed.rb
@@ -1,7 +1,10 @@
-namespace :deploy do 
+namespace :deploy do
     desc "reload the database with seed data"
-    task :seed do 
-        run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}" 
-    end 
+    task :seed do
+        on roles(:all) do
+            within current_path do
+                execute :bundle, :exec, 'rails', 'db:seed', 'RAILS_ENV=production'
+            end
+        end
+    end
 end
-
